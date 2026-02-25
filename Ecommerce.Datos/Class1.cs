e28c094f3d0fa54328b9b36cb73fd1885562d45c
Jaime-MV <jaime.a.matamoros@gmail.com>
2026-02-24 23:44:29 -0600

    Arquitectura inicial: Solución de 3 capas y Área Admin

---
+++ Ecommerce.Datos/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Ecommerce.Datos;
+
+public class Class1
+{
+
+}
---
+++ Ecommerce.Presentacion/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ecommerce.Presentacion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
